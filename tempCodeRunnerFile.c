4e784529e93555abcdc02000a325dbf4ed3984e4
juanruizkk <juanignacioruizr@gmail.com>
2023-03-28 11:07:09 -0300

    Tp terminado

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,8 @@
-pvt++
+ struct compu PC;
+    char tipos[6][10]={"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+    int indice_aleatorio = rand() % 6;
+    PC.velocidad = 1+rand()%3;
+    PC.anio = 2015+rand()%9;
+    PC.cantidad = 1+rand()%9;
+    PC.tipo_cpu = tipos[indice_aleatorio];
+    printf("%s", PC.tipo_cpu); 
